9c2c79938cb2769e9d69d54498f03170493f9d64
unknown <adminadam@adam-surface.wssc.vic.edu.au>
2014-12-04 14:46:47 +1100

    Slimmed down the code, disposing of object to reduce overhead. Post sending back, html file works.

--- Files/init.lua
+++ Files/init.lua
@@ -9,7 +9,7 @@
   html = "text/html"
 }
 
-local requestToConsole = false;
+local requestToConsole = true;
 
 local reqTypes = {
 	GET = true,
@@ -17,8 +17,6 @@ local reqTypes = {
 }
 
 local tags = '<%?lua(.-)%?>';
-local basedir = "/"
-reqdata = {}
 
 local function docode(thecode)
   local strbuf = {};
@@ -35,61 +33,12 @@ local function docode(thecode)
 		print (">>Error: " .. error .. "<<");
   end
   print = oldprint;
-  
+  newprint = nil;
+  oldprint = nil;
+  status = nil;
+  error = nil;
   return table.concat(strbuf);
 end
-
-function process(request, conn)
-
-	local respheader, respdata = '', '';
-	reqdata = {};
-
-	_, _, method, req, major, minor  = string.find(request, "([A-Z]+) (.+) HTTP/(%d).(%d)");
-
-	if reqTypes[method] then
-
-		if method == "POST" then
-			print("post");
-		else
-
-		local fname = ""
-		if req:find("%?") then
-			local rest
-			_, _, fname, rest = req:find("(.*)%?(.*)");
-			rest = rest .. "&";
-			for crtpair in rest:gmatch("[^&]+") do
-				local _, __, k, v = crtpair:find("(.*)=(.*)");
-				-- replace all "%xx" characters with their actual value
-				v = v:gsub("(%%%x%x)", function(s) return string.char(tonumber(s:sub(2, -1), 16)) end);
-				reqdata[k] = v;
-			end
-		else
-			fname = req;
-		end
-
-		fname = ( fname == "/" ) and "index.pht" or fname:sub(2, -1);
-		s, e = fname:find("%.[%a%d]+$");
-		local ftype = fname:sub(s+1, e):lower();
-		ftype = (#ftype > 0 and ftype) or "txt";
-
-		if requestToConsole then
-			print(method .. ":" .. fname);
-		end
-
-		if file.open(fname, "r") then
-			conn:send("HTTP/1.1 200 OK\r\nConnection: close\r\nServer: eLua-miniweb\r\nContent-Type: " .. extmap[ftype or "txt"] .. "\r\n\r\n");
-			sendFileContents(conn,ftype);
-		else
-			conn:send("HTTP/1.1 404 Not Found\r\nConnection: close\r\nServer: eLua-miniweb\r\nContent-Type: text/html\r\n\r\nPage not found");
-		end
-
-		end
-	else
-		conn:send("HTTP/1.1 400 Bad Request\r\nConnection: close\r\nServer: eLua-miniweb\r\nContent-Type: text/html\r\n\r\n400 Invaild Request");
-	end
-
-end
-
 
 sendFileContents = function(conn, type)
 	repeat 
@@ -105,19 +54,75 @@ sendFileContents = function(conn, type)
 	file.close();
 end
 
+responseHeader = function(code, type)
+	return "HTTP/1.1 " .. code .. "\r\nConnection: close\r\nServer: eLua-miniweb\r\nContent-Type: " .. type .. "\r\n\r\n"; 
+end
+
 httpserver = function ()
  srv=net.createServer(net.TCP) 
     srv:listen(80,function(conn) 
-      conn:on("receive",function(conn,payload) 
-        process(payload,conn);
+      conn:on("receive",function(conn,request) 
+
+		local hb = node.heap();
+
+		reqdata = {};
+
+		_, _, method, req, major, minor = string.find(request, "([A-Z]+) (.+) HTTP/(%d).(%d)");
+
+		if reqTypes[method] then
+
+
+			local fname = "";
+			if req:find("%?") then
+				local rest
+				_, _, fname, rest = req:find("(.*)%?(.*)");
+				--rest = rest .. "&";
+				--for crtpair in rest:gmatch("[^&]+") do
+					--local _, __, k, v = crtpair:find("(.*)=(.*)");
+					---- replace all "%xx" characters with their actual value
+					--v = v:gsub("(%%%x%x)", function(s) return string.char(tonumber(s:sub(2, -1), 16)) end);
+					--reqdata[k] = v;
+				--end
+			else
+				fname = req;
+			end
+
+			fname = ( fname == "/" ) and "index.pht" or fname:sub(2, -1);
+
+			s, e = fname:find("%.[%a%d]+$");
+			local ftype = fname:sub(s+1, e):lower();
+			ftype = (#ftype > 0 and ftype) or "txt";
+
+			if file.open(fname, "r") then
+				conn:send(responseHeader("200 OK",extmap[ftype or "txt"]));
+				sendFileContents(conn,ftype);
+			else
+				conn:send(responseHeader("404 Not Found","text/html"));
+				conn:send("Page not found");
+			end
+
+			if requestToConsole then
+				print(method .. ":" .. fname .. ", MemUsage:" .. (hb-node.heap()) .. " (" .. node.heap() .. ")");
+			end
+
+			fname, ftype ,s, e = nil;
+
+		else
+			print("Invaild Request");
+			conn:send(responseHeader("400 Bad Request","text/html"));
+			conn:send("400 Invaild Request");
+		end
+
+		_, method, req, major, minor, request, reqdata, hb = nil, nil, nil, nil, nil, nil, nil, nil;
+
+
       end) 
       conn:on("sent",function(conn) 
-		conn:close()  
-		conn = nil;
+	
+		conn:close(); 
+		conn = nil;	
+
 	  end)
     end)
 end
 httpserver()
-
-
-
